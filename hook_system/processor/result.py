fa34522e213cdd115f6adf71dde9c8485705420d
Dom <domperriccioli@hotmail.com>
2019-02-12 11:19:21 -0500

    created template for processing server's REST API

---
+++ hook_system/processor/result.py
@@ -0,0 +1,4 @@
+
+
+def fetch(userID: str, jobID: str) -> str:
+    return "Here are yer results"
---
+++ hook_system/processor/submit.py
@@ -0,0 +1,4 @@
+
+
+def submission(userID: str, email: str, data: str) -> str:
+    return "Hello User: " + str(userID)
